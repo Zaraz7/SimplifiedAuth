4e6ef333a4f248377153c3086296245a20b13095
Maxim Terlyck <zaraz121801@gmail.com>
2024-02-09 18:57:39 +0300

    Changed version of mod

--- src/main/java/pl/myku/simplifiedAuth/commands/VersionCommand.java
+++ src/main/java/pl/myku/simplifiedAuth/commands/VersionCommand.java
@@ -6,7 +6,7 @@ public class VersionCommand extends Command {
 
     public boolean execute(CommandHandler handler, CommandSender sender, String[] args) {
         if(sender.getPlayer() != null) {
-            sender.sendMessage("Version 0.0.1.2 ");
+            sender.sendMessage("Version 0.0.2.0 ");
             return true;
         }
         else{
