a9668bdcb10278101e3febac45dd8b1a623ea80d
solignomiki <ohmynuts@bk.ru>
2024-08-23 14:03:25 +0600

    Added ResetUserCommand

--- src/main/java/pl/myku/simplifiedAuth/db/IDbManager.java
+++ src/main/java/pl/myku/simplifiedAuth/db/IDbManager.java
@@ -4,6 +4,7 @@ public interface IDbManager {
     Boolean loginPlayer(String username, String password);
     Boolean isPlayerRegistered(String username);
     Boolean isSessionValid(String username, String address);
+    Boolean removePlayerFromDatabase(String username);
     void addPlayerToDatabase(String username, String password);
     void changePassword(String username, String password);
     void updateLastSeenAndAddress(String username, String address);
