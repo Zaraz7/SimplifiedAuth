0aa9e5f36d3121261f20352c7cb60dd8af4c0d5d
solignomiki <ohmynuts@bk.ru>
2025-01-05 20:00:59 +0600

    updated version from 0.5 to 1.0

--- src/main/java/pl/myku/simplifiedAuth/commands/VersionCommand.java
+++ src/main/java/pl/myku/simplifiedAuth/commands/VersionCommand.java
@@ -27,7 +27,7 @@ public class VersionCommand implements CommandManager.CommandRegistry {
                 CommandSource source = (CommandSource)c.getSource();
                 Player player = source.getSender();
                 if(player != null) {
-                    player.sendMessage("Simplified Auth: Version 7.3-0.5");
+                    player.sendMessage("Simplified Auth: Version 7.3-1.0");
                     return Command.SINGLE_SUCCESS;
                 }
                 else{
